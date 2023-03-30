f0a8f7953da25dda5be4371f21dda04f31f29034
zoroskr <99093432+zoroskr@users.noreply.github.com>
2023-03-30 09:43:21 -0300

    Update ejercicio_4.py

--- Practica 2/ejercicio_4.py
+++ Practica 2/ejercicio_4.py
@@ -1,5 +1,4 @@
-evaluar = """ título: Experiences in Developing a Distributed Agent-based
-Modeling Toolkit with Python
+evaluar = """ título: Experiences in Developing a Distributed Agent-based Modeling Toolkit with Python
 resumen: Distributed agent-based modeling (ABM) on high-performance
 computing resources provides the promise of capturing unprecedented details
 of large-scale complex systems. However, the specialized knowledge required
@@ -14,45 +13,27 @@ the largest HPC resources and emerging computing architectures.
 """
 
 #dividimos el texto en palabras
-palabras = evaluar.split()
-
-while (p != 'resumen:'):
-    
-
-
-
-
-
-
-
-cant= 0
-cantResumen= 0
-encontre= False
-for p in palabras:
-    if 'resumen' in p:
-        encontre= True
-    else:
-        if (encontre == False):
-            cant= cant + 1
-        else:
-            cantResumen= cantResumen + 1
-
-if (cant <= 11):
+texto = evaluar.split("\n")
+titulo_separado= texto[0].split()
+if len(titulo_separado) <= 11:
     print('ok')
-else: 
-    print('not ok')
-if (cantResumen <= 12):
-    print('facil de leer')
 else:
-    if (cantResumen >= 13) and (cantResumen <=17):
-        print('aceptalbe')
+    print('not ok')
+evaluar = evaluar.replace(evaluar[0:len(texto[0])],"")
+resumen= evaluar.split(".")
+for i in range(0, len(resumen)):
+    lista_act=resumen[i].split()
+    cantResumen=len(lista_act)
+    if (cantResumen <= 12):
+        print('facil de leer')
     else:
-        if (cantResumen >= 18) and (cantResumen <=25):
-            print('dificil de leer')
+        if (cantResumen >= 13) and (cantResumen <=17):
+            print('aceptalbe')
         else:
-            if (cantResumen > 25):
-                print('muy dificil')
-
-
+            if (cantResumen >= 18) and (cantResumen <=25):
+                print('dificil de leer')
+            else:
+                if (cantResumen > 25):
+                    print('muy dificil')
 
 
